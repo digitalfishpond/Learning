3d86d919cd51c34dd620610e58aff5708e39c522
Denis Poisson <poisson_1@hotmail.com>
2015-09-02 22:24:58 +0200

    Updated SPA

--- AngularJS/14 Weather Forecast SPA/app.js
+++ AngularJS/14 Weather Forecast SPA/app.js
@@ -1,5 +1,25 @@
 var forecastApp = angular.module("forecastApp" , ['ngRoute' , 'ngResource']);
 
-forecastApp.controller = (["$scope" , function($scope){
+forecastApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/homepage', {
+            templateUrl: 'pages/homepage.html',
+            controller: 'homepageController'
+        }).when('/forecastpage', {
+            templateUrl: 'pages/forecastpage.html',
+            controller: 'forecastController'
+        }).otherwise('/homepage', {
+            templateUrl: 'pages/homepage.html',
+            controller: 'homepageController'
+        })
+});
+
+
+
+forecastApp.controller = ('homepageController', ["$scope" , function($scope){
+
+}]);
+
+forecastApp.controller = ('forecastController', ["$scope" , function($scope){
 
 }]);
