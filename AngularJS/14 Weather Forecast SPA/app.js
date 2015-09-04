7292f1b365100c5244d5ba0f0a9814f5ed2b7df3
Denis Poisson <poisson_1@hotmail.com>
2015-09-04 19:30:39 +0200

    updated for api call

--- AngularJS/14 Weather Forecast SPA/app.js
+++ AngularJS/14 Weather Forecast SPA/app.js
@@ -18,13 +18,18 @@ forecastApp.service("cityNameService", function(){
     this.cityName = "Lodz, PL";
 });
 
-forecastApp.controller('homepageController', ["$scope", "cityNameService", function($scope , cityNameService) {
+forecastApp.controller('homepageController', ["$scope", "cityNameService", function($scope , $resource, cityNameService) {
     $scope.cityName = cityNameService.cityName;
     $scope.$watch("cityName" , function(){
        cityNameService.cityName = $scope.cityName;
     });
+
 }]);
 
-forecastApp.controller('forecastController', ["$scope", "cityNameService", function($scope , cityNameService) {
+forecastApp.controller('forecastController', ["$scope", "$resource", "cityNameService", function($scope , $resource, cityNameService) {
     $scope.cityName = cityNameService.cityName;
+
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }} );
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.cityName, cnt: 2});
+    console.log($scope.weatherResult);
 }]);
