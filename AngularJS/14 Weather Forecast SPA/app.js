50e05c2b6368971ebc5661b3e826f8d31d8e7b08
Denis Poisson <poisson_1@hotmail.com>
2015-09-05 15:28:32 +0200

    updated to make api call

--- AngularJS/14 Weather Forecast SPA/app.js
+++ AngularJS/14 Weather Forecast SPA/app.js
@@ -15,21 +15,28 @@ forecastApp.config(function($routeProvider) {
 });
 
 forecastApp.service("cityNameService", function(){
-    this.cityName = "Lodz, PL";
+    this.cityNameFromService = "Lodz, PL";
 });
 
-forecastApp.controller('homepageController', ["$scope", "cityNameService", function($scope , $resource, cityNameService) {
-    $scope.cityName = cityNameService.cityName;
+forecastApp.controller('homepageController', ['$scope', 'cityNameService', function($scope, cityNameService) {
+    console.log(cityNameService.cityNameFromService);
+
+    $scope.cityName = cityNameService.cityNameFromService;
+
     $scope.$watch("cityName" , function(){
-       cityNameService.cityName = $scope.cityName;
+       cityNameService.cityNameFromService = $scope.cityName;
     });
 
 }]);
 
 forecastApp.controller('forecastController', ["$scope", "$resource", "cityNameService", function($scope , $resource, cityNameService) {
-    $scope.cityName = cityNameService.cityName;
+    $scope.cityName = cityNameService.cityNameFromService;
 
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }} );
-    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.cityName, cnt: 2});
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }} );
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.cityName, units: "metric", cnt: 7});
     console.log($scope.weatherResult);
+
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);
+    }
 }]);
