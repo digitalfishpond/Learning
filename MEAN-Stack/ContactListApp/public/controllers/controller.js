71fee41ec872c44236505fbcac800eaa74c6e430
Denis Poisson <poisson_1@hotmail.com>
2015-09-21 22:40:57 +0200

    updated to move data to back end

--- MEAN-Stack/ContactListApp/public/controllers/controller.js
+++ MEAN-Stack/ContactListApp/public/controllers/controller.js
@@ -1,5 +1,10 @@
 myApp.controller('appController', ['$scope', '$http', function($scope, $http){
 
-	console.log("hello from controller")
+	console.log("hello from controller");
+
+	$http.get('/contactlist').success(function (response) {
+		console.log('I got the data I requested');
+		$scope.contactlist = response;
+	});
 
 }]);
--- MEAN-Stack/ContactListApp/server.js
+++ MEAN-Stack/ContactListApp/server.js
@@ -3,5 +3,17 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function (req, res) {
+	console.log('I received a get request');
+
+	var person1 = {name: "Tim", email: "tim@company.com", number: "0208 9957 87 56"};
+	var person2 = {name: "Andrea", email: "andrea@company.com", number: "0208 4578 84 75"};
+	var person3 = {name: "Celine", email: "celine@company.com", number: "0208 5477 55 47"};
+
+	var contactlist = [person1, person2, person3];
+
+	res.json(contactlist);
+});
+
 app.listen(3000);
 console.log("server running on port 3000");
