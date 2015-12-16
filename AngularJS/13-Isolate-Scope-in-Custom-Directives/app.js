c7d1856c470ff830780b773ae045c3c0601305d6
Denis Poisson <poisson_1@hotmail.com>
2015-12-16 12:46:33 +0100

    Update app.js

--- AngularJS/13-Isolate-Scope-in-Custom-Directives/app.js
+++ AngularJS/13-Isolate-Scope-in-Custom-Directives/app.js
@@ -35,6 +35,18 @@ myApp.controller('secondController', ['$scope', '$log', '$routeParams', function
     
     
 }]);
+// By default, directives have access to the model on its parent controller. This can be dangerous. By adding the 'scope' property
+//      as an empty JS object {}, you isolate the directive's scope.
+// If you WANT to access properties from the parent directive:
+// You can specify what properties from the parent controller the custom directive has access to with the following steps.
+// 1. Add a new custom attribute to the tag in which you invoke the directive, 
+// 2. As the value to this new attribute, type the normalised name of the Controller's attribute to be seen.
+// 3. Back in the code of the custom directive, in the 'scope' property's object, add a property with the name 
+// of the new attribute you just created in the html, normalised to camelCase. (person-name --> personName)
+// 4. As the value for this new property, specify a string with one of the four symbols: 
+//  "=" for an object
+//  "@" for text
+//  "&" for a function
 
 myApp.directive("searchResult", function() {
    return {
